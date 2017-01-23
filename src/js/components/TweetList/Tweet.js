dd226fcc36ef2f78a5b4cdb2e224c53a8bd3e993
James Canning <james@jamescanning.com>
2017-01-23 15:15:32 +0000

    Improve quoted tweets positioning 📌

--- src/js/components/TweetList/Tweet.js
+++ src/js/components/TweetList/Tweet.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import cx from 'classnames';
 import TweetHeader from './TweetHeader';
 import TweetPermalink from './TweetPermalink';
 import TweetContent from './TweetContent';
 import TweetAttachment from './TweetAttachment';
 
-function Tweet({ quoted, data }) {
+function Tweet({ isQuoted, data }) {
   const isRetweet = Object.hasOwnProperty.call(data, 'retweeted_status');
+  const hasQuote = Object.hasOwnProperty.call(data, 'quoted_status');
   const tweet = isRetweet ? data.retweeted_status : data;
 
   return (
-    <li className="Tweet">
+    <li className={cx('Tweet', { 'Tweet--quoted': isQuoted })}>
       { isRetweet ? (
         <div className="Tweet__header">
           <span className="Tweet__retweeted">{data.user.name} Retweeted</span>
@@ -19,10 +21,10 @@ function Tweet({ quoted, data }) {
       <div>
         <TweetContent tweet={tweet} />
         <TweetAttachment tweet={tweet} />
-        {tweet.quoted_status ? <div className="Tweet__quoted">
-          <Tweet data={tweet.quoted_status} quoted />
+        {hasQuote ? <div className="Tweet__quoted">
+          <Tweet data={tweet.quoted_status} isQuoted />
         </div> : null}
-        {!quoted ? <TweetPermalink tweet={tweet} /> : null}
+        {!isQuoted ? <TweetPermalink tweet={tweet} /> : null}
       </div>
     </li>
   );
@@ -32,11 +34,11 @@ Tweet.propTypes = {
   data: React.PropTypes.arrayOf(React.PropTypes.shape({
     id_str: React.PropTypes,
   })),
-  quoted: React.PropTypes.boolean,
+  isQuoted: React.PropTypes.boolean,
 };
 
 Tweet.defaultProps = {
-  quoted: false,
+  isQuoted: false,
 };
 
 export default Tweet;

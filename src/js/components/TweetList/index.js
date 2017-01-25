9c6b71e98a666c51ca6d754446006677c8cfaf01
James Canning <james@jamescanning.com>
2017-01-25 12:13:24 +0000

    Add CardList view more links 🍦
    Closes #39

--- src/js/components/TweetList/index.js
+++ src/js/components/TweetList/index.js
@@ -22,6 +22,7 @@ function TweetList({ tweets, isLoading }) {
   return (
     <ul className="TweetList">
       {tweets.map(tweet => <Tweet data={tweet} key={tweet.id_str} />)}
+      <a className="TweetList__view-more" href="https://twitter.com/USSU/lists/ussu">View on Twitter</a>
     </ul>
   );
 }

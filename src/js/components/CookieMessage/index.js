b7a4f94c805c8b02a91749e2dc535419d4f126cc
James Canning <james@jamescanning.com>
2017-01-25 10:17:28 +0000

    Add space to CookieMessage 👽
    Closes #49

--- src/js/components/CookieMessage/index.js
+++ src/js/components/CookieMessage/index.js
@@ -20,7 +20,7 @@ class CookieMessage extends React.Component {
 
     return (
       <div className="GlobalNotice">
-        This site uses cookies. By continuing to browse, you agree to our use of cookies on the site.
+        {'This site uses cookies. By continuing to browse, you agree to our use of cookies on the site. '}
         <a href="/privacy-policy">Learn more</a>.
         <button className="GlobalNotice__close" title="Close" onClick={this.handleHide}>
           <span className="u-h">

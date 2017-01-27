7c0b1a492e8be50e26dd312f26feeb1906270bdf
James Canning <james@jamescanning.com>
2017-01-27 16:57:42 +0000

    Add getinvolved page generation 🏅

---
+++ generator/layouts/getinvolved.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import FigureCollection from '../components/FigureCollection';
+import FigureCollectionFigure from '../components/FigureCollectionFigure';
+import HomepageEventsList from '../components/HomepageEventsList';
+
+// cafe
+//
+
+// elected
+// /pageassets/get-involved/Group-1.jpg
+
+const createItem = (title, link, imageURL) => ({ title, link, imageURL });
+const items = [
+  createItem(
+    'Sport, societies & media',
+    '/sport-societies-media',
+    '/pageassets/get-involved/ssm.jpg',
+  ),
+  createItem(
+    'Join the Role Models Project',
+    '/volunteer/role-models',
+    '/pageassets/get-involved/role-models.jpg',
+  ),
+  createItem(
+    'Volunteer at reception',
+    '/volunteer/reception',
+    '/pageassets/get-involved/Rhiannon.jpg',
+  ),
+  createItem(
+    'Join the Welcome Team',
+    '/volunteer/welcome-team',
+    '/pageassets/get-involved/couple-at-the-beach.jpg',
+  ),
+  createItem(
+    'Join a campaign team',
+    '/volunteer/campaign-volunteer',
+    '/pageassets/get-involved/campaign-team.jpg',
+  ),
+  createItem(
+    'Volunteer at the Language Café',
+    '/volunteer/language-cafe',
+    '/pageassets/get-involved/d5mqnyolclw-alisa-anton.jpg',
+  ),
+  createItem(
+    'Join the Buddy Scheme',
+    'http://www.buddyscheme.com/apply',
+    '/pageassets/get-involved/buddy-scheme.jpg',
+  ),
+  createItem(
+    'Hold an elected position',
+    '/elections',
+    '/pageassets/get-involved/elected-position.jpg',
+  ),
+  createItem(
+    'Campaign for change',
+    '/campaigns',
+    '/pageassets/get-involved/campaign-change.jpg',
+  ),
+  createItem(
+    'Vote',
+    '/democracy',
+    '/pageassets/get-involved/vote.jpg',
+  ),
+  createItem(
+    'Work for us',
+    '/jobs',
+    '/pageassets/get-involved/work.jpg',
+  ),
+];
+
+const img = '/pageassets/get-involved/get-involved.jpg';
+const GetInvolved = () => (
+  <div className="Container">
+    <div className="HeadingImage" style={{ backgroundImage: `url(${img}?thumbnail=true&height=700&width=2000&resize_type=CropToFit)` }}>
+      <h1 className="HeadingImage__title">Get involved</h1>
+    </div>
+    <ul className="SelectionGrid">
+      {items.map(item => (
+        <li className="SelectionGrid__item SelectionGrid--underneath">
+          <a className="SelectionGrid__link" href={item.link}>
+            <div className="SelectionGrid__image" style={{ backgroundImage: `url(${item.imageURL}?thumbnail=true&height=230&width=640&resize_type=CropToFit)` }}>
+              <div className="SelectionGrid__image-inside">
+                <div className="SelectionGrid__title">{item.title}</div>
+              </div>
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default GetInvolved;

a9b44458b7efaedc116caeb09d47244e666c3ffd
James Canning <james@jamescanning.com>
2017-02-24 15:51:17 +0000

    Tidy up scripts, move to package.json ⛱

--- scripts/deploydev.sh
+++
@@ -1,7 +0,0 @@
-npm run prod;
-s3cmd put --recursive ./dist/ s3://ussu-static/ --config=.s3cfg --acl-public
-node ./scripts/combine-prod-wintersmith-assets.js
-(cd ./prototypes && `npm bin`/wintersmith build --config ./config-prod.json);
-mkdir ./prototypes/build/assets
-mv ./dist/* ./prototypes/build/assets/;
-(cd ./prototypes/build && surge --domain ussu.surge.sh)
--- scripts/deployprod.sh
+++
@@ -1,3 +0,0 @@
-npm run prod;
-s3cmd put --recursive --add-header="Cache-Control: public, max-age=365000000, immutable" ./dist/ s3://ussu-static/ --config=.s3cfg --acl-public
-npm run gen;

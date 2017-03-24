b513962b1f746662aab8f77b7595eb9d72ee12e2
James Canning <james@jamescanning.com>
2017-03-24 14:13:23 +0000

    🤕 Add container bleed helpers
    Closes #137

--- generator/components/HomepageEventsList.js
+++ generator/components/HomepageEventsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomepageEventsList = () => (
-  <div className="EventsList">
+  <div className="EventsList u-container-bleed-1">
     {'{alleventslist}'}
     <a className="EventsList__view-more" href="/whats-on">See all events</a>
   </div>

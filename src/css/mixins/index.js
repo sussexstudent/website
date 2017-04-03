6cdb943a7bc965281d680ab196af9dd0ca935099
James Canning <james@jamescanning.com>
2017-04-03 14:10:18 +0100

    Add initial version of new search

--- src/css/mixins/index.js
+++ src/css/mixins/index.js
@@ -5,10 +5,28 @@ module.exports = {
         return {
           '&': {
             'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
+            'border-radius': '2px',
             transition: 'box-shadow 300ms ease',
           },
           '&:hover': {
             'box-shadow': '0 3px 5px 0 rgba(0, 0, 0, 0.15);',
+          },
+          '&:active': {
+            'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
+          },
+        };
+      }
+      case 'standard-no-transition': {
+        return {
+          '&': {
+            'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
+            'border-radius': '2px',
+          },
+          '&:hover': {
+            'box-shadow': '0 3px 5px 0 rgba(0, 0, 0, 0.15);',
+          },
+          '&:active': {
+            'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
           },
         };
       }

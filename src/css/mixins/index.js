20b79bb6ba8cd8d745cb55e1aee9cbac11b74e86
James Canning <james@jamescanning.com>
2017-04-07 14:30:36 +0100

    ⭕️ Add rounded radius - design parity
    Closes #150

--- src/css/mixins/index.js
+++ src/css/mixins/index.js
@@ -1,4 +1,12 @@
 module.exports = {
+  rounded: function roundedMixin(mixin, type) {
+    return {
+      '&': {
+        'border-radius': '2px',
+        overflow: 'hidden',
+      }
+    }
+  },
   card: function cardMixin(mixin, type) {
     switch (type) {
       case 'standard': {
@@ -6,6 +14,7 @@ module.exports = {
           '&': {
             'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
             'border-radius': '2px',
+            overflow: 'hidden',
             transition: 'box-shadow 300ms ease',
           },
           '&:hover': {

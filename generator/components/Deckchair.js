ca0db91e811cf9f9b8a3b095614563e3a1379401
James Canning <james@jamescanning.com>
2017-04-10 16:29:03 +0100

    ♻️ Add headingimage to envrioment student feedback

--- generator/components/Deckchair.js
+++ generator/components/Deckchair.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Deckchair = ({ header, about, buttonTitle, buttonAction, buttonLink, chairKey, color }) => (
   <div className={`Deckchair Deckchair--color-${color}`} data-chair={chairKey}>
-    <div className="Deckchair__close" />
+    {chairKey ? <div className="Deckchair__close" /> : null}
     <div className="Deckchair__info">
       <h3 className="Deckchair__header">{header}</h3>
       <p className="Deckchair__about">{about}</p>

4ea2b8aecee66eb3b0a5fc1d89e0dd791a1f3e8d
James Canning <james@jamescanning.com>
2017-04-11 11:57:16 +0100

    🎯 Use msl-utils for news parsing
    Closes #158

--- src/js/components/NewsList/NewsBlock.js
+++ src/js/components/NewsList/NewsBlock.js
@@ -3,7 +3,7 @@ import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 import isToday from 'date-fns/is_today';
 import Logotype from '../../../img/logotype';
 
-const NewsBlock = ({ item: { title, link, pubDate, led, imageURL = null } }) => (
+const NewsBlock = ({ item: { title, link, publishedDate, led, imageURL = null } }) => (
   <li className="NewsGrid__item NewsBlock">
     <a className="NewsBlock__link" href={link}>
       {imageURL && <div className="NewsBlock__image" style={{ backgroundImage: `url(${imageURL})` }} />}
@@ -18,7 +18,9 @@ const NewsBlock = ({ item: { title, link, pubDate, led, imageURL = null } }) => 
         </div>
         <p className="NewsBlock__standfirst">{led}</p>
         <div className="NewsBlock__meta">
-          <date className="NewsBlock__date">{isToday(pubDate) ? 'Today' : `${distanceInWordsToNow(pubDate)} ago`}</date>
+          <date className="NewsBlock__date">
+            {isToday(publishedDate) ? 'Today' : `${distanceInWordsToNow(publishedDate)} ago`}
+          </date>
         </div>
       </div>
     </a>
@@ -29,7 +31,7 @@ NewsBlock.propTypes = {
   item: React.PropTypes.shape({
     title: React.PropTypes.string.isRequired,
     link: React.PropTypes.string.isRequired,
-    pubDate: React.PropTypes.instanceOf(Date).isRequired,
+    publishedDate: React.PropTypes.instanceOf(Date).isRequired,
     led: React.PropTypes.string.isRequired,
     imageURL: React.PropTypes.string,
   }).isRequired,

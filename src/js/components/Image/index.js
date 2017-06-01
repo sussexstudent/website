c7d7e80c808abe6598d65e98ec10cd928d7f092c
James Canning <james@jamescanning.com>
2017-06-01 10:07:17 +0100

    Increase image precision
    Some images had bad cropping due to 100px prescision causing drastic aspect ratio changes

--- src/js/components/Image/index.js
+++ src/js/components/Image/index.js
@@ -4,7 +4,7 @@ import Imgix from 'react-imgix';
 const ENDPOINT = 'https://su.imgix.net/';
 
 function Image(props) {
-  return <Imgix {...props} src={`${ENDPOINT}/${props.src}`} />;
+  return <Imgix precision={20} {...props} src={`${ENDPOINT}/${props.src}`} />;
 }
 
 export default Image;

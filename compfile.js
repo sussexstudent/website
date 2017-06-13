72fc728b0e0513dc52fdb8e84f4905d0f4927caa
James Canning <james@brudil.com>
2017-06-13 15:04:57 +0100

    Update comp to v1

--- compfile.js
+++ compfile.js
@@ -2,15 +2,12 @@ require('isomorphic-fetch');
 
 import path from 'path';
 import React from 'react';
-import MainLayout from './generator/layouts/main';
 import { headContent, headContentLegacy } from './generator/head';
 import Html from './generator/layouts/Html';
 import assets from './webpack-assets.json';
 import manifest from './dist/manifest.json';
 
 assets.manifest = manifest;
-
-const MainLayoutLegacy = props => <MainLayout {...props} legacy />;
 
 // TODO: NO COMPONENT IMPORTS IN THIS FILE, USE PATHS. IMPORT ON DEMAND IN COMP.
 
@@ -20,23 +17,23 @@ export default {
   templates: {
     main: {
       head: headContent,
-      templatePublic: MainLayout,
-      templateLoggedIn: MainLayout,
+      templatePublic: './layouts/main.js',
+      templateLoggedIn: './layouts/main.js',
     },
     'main-containerless': {
       head: headContent,
-      templatePublic: MainLayout,
-      templateLoggedIn: MainLayout,
+      templatePublic: './layouts/main.js',
+      templateLoggedIn: './layouts/main.js',
     },
     legacy: {
       head: headContent,
-      templatePublic: MainLayoutLegacy,
-      templateLoggedIn: MainLayoutLegacy,
+      templatePublic: './layouts/MainLegacy.js',
+      templateLoggedIn: './layouts/MainLegacy.js',
     },
     'legacy-jquery': {
       head: headContentLegacy,
-      templatePublic: MainLayoutLegacy,
-      templateLoggedIn: MainLayoutLegacy,
+      templatePublic: './layouts/MainLegacy.js',
+      templateLoggedIn: './layouts/MainLegacy.js',
     },
   },
   pages: {

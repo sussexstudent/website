ac0471c78bddd439210a81aa44d4ecd2f148fc3c
James Canning <james@brudil.com>
2017-06-20 22:35:46 +0100

    Add BackBar snapshot

---
+++ src/js/components/BackBar/BackBar.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import BackBar from './index';
+
+it('renders a snapshot', () => {
+  const tree = renderer
+    .create(<BackBar href="https://example.com">All examples</BackBar>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('accepts colors', () => {
+  const tree = renderer
+    .create(
+      <BackBar href="https://example.com" color="red">All examples</BackBar>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});

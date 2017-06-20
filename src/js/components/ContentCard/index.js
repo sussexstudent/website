5b3d471c9dbf0466afca0c6ef4388fd53b1c8a0d
James Canning <james@brudil.com>
2017-06-20 10:44:51 +0100

    Move ContentCard to components dir

---
+++ src/js/components/ContentCard/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContentCard = ({ anchor = null, children }) => (
+  <div className="ContentCard">
+    {anchor !== null
+      ? <span className="u-position-anchor" id={anchor} />
+      : null}
+    {children}
+  </div>
+);
+
+ContentCard.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+ContentCard.description = `
+  A simple card for seperating different sections visually.
+`;
+
+ContentCard.examples = {
+  standard: (
+    <ContentCard>
+      <p>Hello there!</p>
+    </ContentCard>
+  ),
+};
+
+ContentCard.level = 'block';
+
+export default ContentCard;

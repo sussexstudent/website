5477bf8bc4d16d4d0c49308149db78efaa2f8d86
James Canning <james@brudil.com>
2017-06-20 22:33:11 +0100

    Update officers on homepage

--- generator/layouts/homepage.js
+++ generator/layouts/homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import FigureCollection from '../components/FigureCollection';
-import FigureCollectionFigure from '../components/FigureCollectionFigure';
-import HomepageEventsList from '../components/HomepageEventsList';
+import FC from '@ussu/components/FigureCollection';
+import HydroLeaf from '@ussu/components/HydroLeaf';
 import TweetList from '@ussu/components/TweetList';
+import HomepageEventsList from '../components/HomepageEventsList';
 import { Hydro as HeadingHero } from '../../src/js/components/HeadingHero/index';
+
+const FigureCollection = HydroLeaf()(FC);
 
 const Homepage = () =>
   <div>
@@ -43,44 +45,54 @@ const Homepage = () =>
 
       <div className="ContentBlock">
         <div className="ContentBlock__heading">Your officers</div>
-        <FigureCollection>
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/Adele(1).jpg"
-            title="Adèle Duvillier"
-            sub="Activities Officer"
-            link="/about-us/full-time-elected-officers/activities"
-          />
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/Sarah-bw.jpg"
-            title="Sarah Gibbons"
-            sub="Society and Citizenship Officer"
-            link="/about-us/full-time-elected-officers/society-citizenship"
-          />
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/Savannah(1).jpg"
-            title="Savannah Sevenzo"
-            sub="Undergraduate Education Officer"
-            link="/about-us/full-time-elected-officers/undergraduate-education"
-          />
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/Rose-bw.jpg"
-            title="Rose Taylor"
-            sub="Postgraduate Education Officer"
-            link="/about-us/full-time-elected-officers/postgraduate-education"
-          />
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/Annie-bw.jpg"
-            title="Annie Pickering"
-            sub="President"
-            link="/about-us/full-time-elected-officers/president"
-          />
-          <FigureCollectionFigure
-            imageURL="https://www.sussexstudent.com/pageassets/about-us/full-time-elected-officers/on-white.jpg"
-            title="Grainne Gahan"
-            sub="Welfare Officer"
-            link="/about-us/full-time-elected-officers/welfare"
-          />
-        </FigureCollection>
+        <FigureCollection
+          items={[
+            {
+              imageResource:
+                'images/4759cccd3b3f4ed4813a260cc0d4833f.original.jpg',
+              title: 'Lucy Williams',
+              sub: 'Activities Officer',
+              link: '/about-us/full-time-elected-officers/activities',
+            },
+            {
+              imageResource:
+                'images/8da8156f2e0e40b4878dcbea569ce2b5.original.jpg',
+              title: 'Aisling Murray',
+              sub: 'Society and Citizenship Officer',
+              link: '/about-us/full-time-elected-officers/society-citizenship',
+            },
+            {
+              imageResource:
+                'images/241b436e49524d8da68faf1f1f83c4b6.original.jpg',
+              title: 'Lulah Stratheran Brady',
+              sub: 'Undergraduate Education Officer',
+              link:
+                '/about-us/full-time-elected-officers/undergraduate-education',
+            },
+            {
+              imageResource:
+                'images/f07caf2bb1114cb8a18dd49a6e9e1045.original.jpg',
+              title: 'Sarah McIntosh',
+              sub: 'Postgraduate Education Officer',
+              link:
+                '/about-us/full-time-elected-officers/postgraduate-education',
+            },
+            {
+              imageResource:
+                'images/16664de397694b5ea5e194b0a1fa73dc.original.jpg',
+              title: 'Frida Gustafsson',
+              sub: 'President',
+              link: '/about-us/full-time-elected-officers/president',
+            },
+            {
+              imageResource:
+                'images/d05f369bcabc47ed83ecaac0e36a9adb.original.jpg',
+              title: 'Grainne Gahan',
+              sub: 'Welfare Officer',
+              link: '/about-us/full-time-elected-officers/welfare',
+            },
+          ]}
+        />
       </div>
     </div>
   </div>;

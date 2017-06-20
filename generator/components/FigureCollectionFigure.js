4cff0b13678ca5fce0ab0e9db6be8196f29a3119
James Canning <james@brudil.com>
2017-06-20 22:32:53 +0100

    Use Image component in FigureColectionFigures

--- generator/components/FigureCollectionFigure.js
+++ generator/components/FigureCollectionFigure.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import Image from '@ussu/components/Image';
 import PropTypes from 'prop-types';
 
-const FigureCollectionFigure = ({ imageURL, title, sub, link }) =>
+const FigureCollectionFigure = ({ imageResource, title, sub, link }) =>
   <li className="FigureCollection__item">
     <a href={link} className="FigureCollection__link">
-      <img
-        className="FigureCollection__image"
-        src={`${imageURL}?thumbnail=true&height=400&width=400&resize_type=CropToFit`}
-        alt=""
-      />
+      <div className="u-responsive-ratio u-responsive-ratio--square">
+        <Image className="ResponsiveImage" alt="" src={imageResource} />
+      </div>
       <span className="FigureCollection__title">{title}</span>
       <span className="FigureCollection__secondary">{sub}</span>
     </a>

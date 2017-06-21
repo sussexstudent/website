43add093d652fd426e3b376bdfc2035bd9f38c3e
James Canning <james@brudil.com>
2017-06-21 11:01:31 +0100

    Add Button component

---
+++ src/js/components/Button/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import cx from 'classnames';
+
+function Button({ href, children }) {
+  return <a className={cx('Button')} href={href}>{children}</a>;
+}
+
+export default Button;

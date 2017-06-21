9bdaff921e341e55f8616fee5184a59b5e4f04c4
James Canning <james@brudil.com>
2017-06-21 10:59:16 +0100

    Don't use Imgix component within tests
    Due to use of findDOMNode not being supported within Jest snapshots

--- src/js/components/Image/index.js
+++ src/js/components/Image/index.js
@@ -4,6 +4,18 @@ import Imgix from 'react-imgix';
 const ENDPOINT = 'https://su.imgix.net/';
 
 function Image(props) {
+  if (process.env.NODE_ENV === 'test') {
+    /*
+      TODO: hacky and probally makes tests a bit useless
+      all due to findDOMNode in the Imgix component
+    */
+    return (
+      <div className="TEST_IMAGE" data-props={props.src}>
+        {props.children ? props.children : null}
+      </div>
+    );
+  }
+
   return <Imgix precision={20} {...props} src={`${ENDPOINT}/${props.src}`} />;
 }
 

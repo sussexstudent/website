fac9ebc4e0880448a0cc5aaa94a2221efb2556b4
James Canning <james@brudil.com>
2017-06-21 11:00:48 +0100

    Add Storyshot build snapshots

--- src/js/components/NewsList/index.js
+++ src/js/components/NewsList/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import NewsBlock from './NewsBlock';
 
-const NewsList = ({ items }) =>
-  <ul className="NewsGrid">
+const NewsList = ({ items, fullWidth = false }) =>
+  <ul className={cx('NewsGrid', { 'NewsGrid--full-width': fullWidth })}>
     {items.map(item => <NewsBlock item={item} key={item.id} />)}
   </ul>;
 

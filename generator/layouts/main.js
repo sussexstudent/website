4b30c15c85b7be390205d53cfc947c3d39e7e14c
James Canning <james@brudil.com>
2017-06-26 09:58:03 +0100

    Add Object.entries to polyfill

--- generator/layouts/main.js
+++ generator/layouts/main.js
@@ -34,7 +34,7 @@ const mainLayout = ({ loggedIn, assets, legacy }) =>
       <AdvertBar position="lozenge1" dark />
     </div>
     <div className="js__modal" />
-    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?rum=0&features=es6,es7,default-3.6,performance.now&flags=gated&callback=hasPolyfilled&unknown=polyfill" />
+    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?rum=0&features=es6,es7,default-3.6,performance.now,Object.entries&flags=gated&callback=hasPolyfilled&unknown=polyfill" />
     <script src={assets.vendor.js} />
     <script src={assets.main.js} />
     <script type="text/javascript" dangerouslySetInnerHTML={{ __html: ga }} />

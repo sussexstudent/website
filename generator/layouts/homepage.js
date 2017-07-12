97041d8917397782cceacef98ef723e09160a6bd
James Canning <james@brudil.com>
2017-07-12 17:03:44 +0100

    Add Homepage AdvertBar for mobile

--- generator/layouts/homepage.js
+++ generator/layouts/homepage.js
@@ -4,11 +4,13 @@ import HydroLeaf from '@ussu/components/HydroLeaf';
 import TweetList from '@ussu/components/TweetList';
 import HomepageEventsList from '../components/HomepageEventsList';
 import { Hydro as HeadingHero } from '../../src/js/components/HeadingHero/index';
+import AdvertBar from '../components/AdvertBar';
 
 const FigureCollection = HydroLeaf()(FC);
 
 const Homepage = () =>
   <div>
+    <AdvertBar className="AdvertBar--mobile-home" position="TopBanner" />
     <div className="Trail">
       <div className="Trail__row  u-container-bleed-2">
         <a
@@ -16,8 +18,8 @@ const Homepage = () =>
           href="https://www.sussexstudent.com/our-campus-services/"
         >
           <HeadingHero
-            imageURL="/images/4219b2966c1047dd8fe4bfa2aa922c72.original.jpg"
-            title={'Our holiday opening times'}
+            imageURL="/images/ec00cc188009492f868d14ac9a228b97.original.jpg"
+            title={'Last chance: The Horrors tickets on sale now'}
           />
         </a>
       </div>
@@ -48,7 +50,7 @@ const Homepage = () =>
       </div>
 
       <div className="ContentBlock">
-        <div className="ContentBlock__heading">Your officers</div>
+        <div className="ContentBlock__heading">Your full-time officers</div>
         <FigureCollection
           items={[
             {
@@ -94,6 +96,49 @@ const Homepage = () =>
               title: 'Grainne Gahan',
               sub: 'Welfare Officer',
               link: '/about-us/full-time-elected-officers/welfare',
+            },
+          ]}
+        />
+      </div>
+
+      <div className="ContentBlock">
+        <div className="ContentBlock__heading">Your part-time officers</div>
+        <FigureCollection
+          items={[
+            {
+              imageResource:
+                'images/4759cccd3b3f4ed4813a260cc0d4833f.original.jpg',
+              title: 'Simin Wadiwala',
+              sub: 'Ethnic Minority Students Officer',
+              link: '/democracy/part-time-officer-elections/',
+            },
+            {
+              imageResource:
+                'images/8da8156f2e0e40b4878dcbea569ce2b5.original.jpg',
+              title: 'Liz Gladden',
+              sub: 'Women Students Officer',
+              link: '/democracy/part-time-officer-elections/',
+            },
+            {
+              imageResource:
+                'images/241b436e49524d8da68faf1f1f83c4b6.original.jpg',
+              title: 'Chris Lewis',
+              sub: 'LGBTQ+ Students Officer',
+              link: '/democracy/part-time-officer-elections/',
+            },
+            {
+              imageResource:
+                'images/f07caf2bb1114cb8a18dd49a6e9e1045.original.jpg',
+              title: 'Fi Halfacre',
+              sub: 'Students with Disabilities Officer',
+              link: '/democracy/part-time-officer-elections/',
+            },
+            {
+              imageResource:
+                'images/16664de397694b5ea5e194b0a1fa73dc.original.jpg',
+              title: 'Henry Tee',
+              sub: 'Trans and Non-Binary Students Officer',
+              link: '/democracy/part-time-officer-elections/',
             },
           ]}
         />

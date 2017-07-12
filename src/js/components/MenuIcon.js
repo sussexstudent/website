ad06e4cf32a7fb4d4fd1994ce8f612f6168c2d43
James Canning <james@brudil.com>
2017-07-12 10:36:20 +0100

    Fix sizes of Header icons

--- src/js/components/MenuIcon.js
+++ src/js/components/MenuIcon.js
@@ -3,23 +3,16 @@ import React from 'react';
 function MenuIcon() {
   return (
     <svg
-      width="22px"
-      height="15px"
-      viewBox="0 0 22 15"
-      version="1.1"
       xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="21"
+      viewBox="0 0 23 21"
     >
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g transform="translate(-272.000000, -15.000000)" fill="#323232">
-          <g id="Group-7" transform="translate(268.000000, 15.000000)">
-            <g id="Group-4" transform="translate(4.000000, 0.000000)">
-              <path
-                d="M2.45,3 L0.95,3 L0.95,0 L2.45,0 L19.55,0 L21.05,0 L21.05,3 L19.55,3 L2.45,3 Z M6.45,9 L4.95,9 L4.95,6 L6.45,6 L19.5,6 L21,6 L21,9 L19.5,9 L6.45,9 Z M2.45,15 L0.95,15 L0.95,12 L2.45,12 L19.55,12 L21.05,12 L21.05,15 L19.55,15 L2.45,15 Z"
-                id="Combined-Shape"
-              />
-            </g>
-          </g>
-        </g>
+      <g fill="none" fillRule="evenodd">
+        <path
+          fill="#323232"
+          d="M2.567 6H1V3h21v3H2.567zm4.022 6H5V9h17v3H6.589zm-4.022 6H1v-3h21v3H2.567z"
+        />
       </g>
     </svg>
   );

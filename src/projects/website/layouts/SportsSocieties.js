6a6b25ce8e4607047c6b10ec4f628e5e8fcddcb3
James Canning <james@brudil.com>
2017-08-22 11:38:52 +0100

    Add back bar to SGD

--- src/projects/website/layouts/SportsSocieties.js
+++ src/projects/website/layouts/SportsSocieties.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import StudentGroupsDiscovery from '~components/StudentGroupsDiscovery';
+import BackBar from '~components/BackBar';
 
 function SportsSocieties() {
   return (
     <div>
+      <BackBar href="/get-involved" color="blue">
+        Get involved
+      </BackBar>
       <div className="Layout">
         <div>
           <StudentGroupsDiscovery />

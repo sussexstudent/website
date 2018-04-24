952649d8d7373e1c9542cf97f421a45316b4d623
James Canning <james@brudil.com>
2018-04-24 13:22:06 +0100

    disable redirect for now

--- src/modules/eventRedirect.ts
+++ src/modules/eventRedirect.ts
@@ -1,4 +1,4 @@
-import getFalmerEndpoint from '~libs/getFalmerEndpoint';
+// import getFalmerEndpoint from '~libs/getFalmerEndpoint';
 import { parseTickets } from '~libs/mslEventTicketParser';
 import user from '~libs/user';
 
@@ -26,40 +26,40 @@ function eventPage() {
 
       return;
     }
-
-    try {
-      const mslEventId = parseInt(match[1], 10);
-
-      fetch(`${getFalmerEndpoint()}/graphql/`, {
-        method: 'post',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          query: `
-          query FalmerEventPath($mslEventId: Int) {
-            event(mslEventId: $mslEventId) {
-              slug
-              eventId
-            }
-          }
-        `,
-          variables: {
-            mslEventId,
-          },
-        }),
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          if (response.data.event) {
-            const { slug, eventId } = response.data.event;
-            window.history.replaceState({}, '', `/whats-on/${slug}-${eventId}`);
-            window.location.reload();
-          }
-        });
-    } catch (e) {
-      // do nothing
-    }
+    //
+    // try {
+    //   const mslEventId = parseInt(match[1], 10);
+    //
+    //   fetch(`${getFalmerEndpoint()}/graphql/`, {
+    //     method: 'post',
+    //     headers: {
+    //       'content-type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       query: `
+    //       query FalmerEventPath($mslEventId: Int) {
+    //         event(mslEventId: $mslEventId) {
+    //           slug
+    //           eventId
+    //         }
+    //       }
+    //     `,
+    //       variables: {
+    //         mslEventId,
+    //       },
+    //     }),
+    //   })
+    //     .then((res) => res.json())
+    //     .then((response) => {
+    //       if (response.data.event) {
+    //         const { slug, eventId } = response.data.event;
+    //         window.history.replaceState({}, '', `/whats-on/${slug}-${eventId}`);
+    //         window.location.reload();
+    //       }
+    //     });
+    // } catch (e) {
+    //   // do nothing
+    // }
   }
 }
 

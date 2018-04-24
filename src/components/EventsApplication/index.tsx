de8ae03b04d200686a0806a3c7a11a4819199720
James Canning <james@brudil.com>
2018-04-24 13:01:53 +0100

    What's on BackBar scroll restoration
    closes #330

--- src/components/EventsApplication/index.tsx
+++ src/components/EventsApplication/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { compose } from 'recompose';
+import bind  from 'bind-decorator';
+import { debounce } from 'lodash';
+import { Route, Switch, withRouter, RouteComponentProps } from 'react-router-dom';
 import HydroLeaf from '~components/HydroLeaf';
 import Loadable from 'react-loadable';
 import { LoadableLoading } from '~components/LoadableLoading';
@@ -15,7 +16,7 @@ const LoadableListings = Loadable({
   render({ EventsList }: { EventsList: any }, props) {
     return <EventsList {...props} />;
   },
-});
+}) as any;
 
 const LoadableListingsBranding = Loadable({
   loading: LoadableLoading,
@@ -23,7 +24,7 @@ const LoadableListingsBranding = Loadable({
   render({ EventsBrandingPeriod }: { EventsBrandingPeriod: any }, props) {
     return <EventsBrandingPeriod {...props} />;
   },
-});
+}) as any;
 
 const LoadableDetail = Loadable({
   loading: LoadableLoading,
@@ -31,29 +32,65 @@ const LoadableDetail = Loadable({
     import(/* webpackChunkName: "events.detail" */ '~components/EventDetailPage'),
 });
 
-const EventsApplication = () => (
-  <div className="u-keep-footer-down">
-    <Switch>
-      <Route path="/whats-on/" exact component={LoadableListings} />
-      <Route
-        path="/whats-on/period/:brandSlug"
-        exact
-        component={LoadableListingsBranding}
-      />
-      <Route
-        path="/whats-on/collection/:brandSlug"
-        exact
-        component={LoadableListingsBranding}
-      />
-      <Route path="/whats-on/**-:eventId" component={LoadableDetail} />
-      <Route path="/whats-on/:eventId" component={LoadableDetail} />
-    </Switch>
-  </div>
-);
+interface Props  {
+}
 
-export default compose(
-  HydroLeaf({
-    disableSSR: true,
-    providers: [Provider.Apollo, Provider.Router],
-  }),
-)(EventsApplication);
+type EventsApplicationProps = RouteComponentProps<{}> & Props;
+
+interface EventsApplicationState {
+  currentListingsScrollPosition: number;
+}
+
+class EventsApplication extends React.Component<EventsApplicationProps, EventsApplicationState> {
+  state = {
+    currentListingsScrollPosition: -1,
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', debounce(this.onScroll, 100));
+  }
+
+  @bind
+  onScroll() {
+    if (this.props.location.pathname === '/whats-on' && window.scrollY !== 0) {
+      this.setState({ currentListingsScrollPosition: window.scrollY });
+    }
+  }
+
+  componentDidUpdate(prevProps: EventsApplicationProps) {
+    if (
+      this.props.location.pathname === '/whats-on'
+      && this.props.location.pathname !== prevProps.location.pathname
+      && this.state.currentListingsScrollPosition !== null
+    ) {
+      requestAnimationFrame(() => window.scrollTo({ top: this.state.currentListingsScrollPosition }))
+    }
+  }
+
+  render() {
+    return (
+      <div className="u-keep-footer-down">
+        <Switch>
+          <Route path="/whats-on/" exact component={LoadableListings} />
+          <Route
+            path="/whats-on/period/:brandSlug"
+            exact
+            component={LoadableListingsBranding}
+          />
+          <Route
+            path="/whats-on/collection/:brandSlug"
+            exact
+            component={LoadableListingsBranding}
+          />
+          <Route path="/whats-on/**-:eventId" component={LoadableDetail} />
+          <Route path="/whats-on/:eventId" component={LoadableDetail} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default HydroLeaf({
+  disableSSR: true,
+  providers: [Provider.Apollo, Provider.Router],
+})(withRouter(EventsApplication as any));

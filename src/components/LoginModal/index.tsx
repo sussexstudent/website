cc70fe65203ea6063f3439324f8a1d884e488950
James Canning <james@brudil.com>
2018-06-15 13:24:01 +0100

    Basic login modal
    Still uses /login. Part of #76

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Modal } from '~components/Modal';
-import Button from '~components/Button';
 
 interface LoginModalProps {}
 
@@ -8,27 +7,46 @@ export class LoginModal extends React.Component<
   LoginModalProps & ReactModal.Props
 > {
   render() {
-    const currentPath = '/'; // window.location.pathname;
+    const currentPath = typeof window !== "undefined" ? window.location.pathname : '/';
 
     return (
-      <Modal isOpen={true} size="small" {...this.props}>
-        <h1>Log in</h1>
-        <ul>
+      <Modal isOpen={true} size="small" {...this.props} footerClose>
+        <h1 className="Modal__heading">Log in</h1>
+        <ul className="LoginActions">
           <li>
-            <Button
+            <a
+              className="LoginActions__item-anchor"
               href={`https://www.sussexstudent.com/sso/login.ashx?idp=SUSSEX_SHIB&ReturnUrl=${currentPath}`}
             >
               Sussex SSO
-            </Button>
+            </a>
           </li>
           <li>
-            <Button
+            <a
+              className="LoginActions__item-anchor"
               href={`https://www.sussexstudent.com/sso/login.ashx?idp=BSMS_SHIB&ReturnUrl=${currentPath}`}
             >
               BSMS SSO
-            </Button>
+            </a>
+          </li>
+          <li>
+            <a
+              className="LoginActions__item-anchor"
+              href={`https://www.sussexstudent.com/login`}
+            >
+              Study Group & Gothenburg students
+            </a>
+          </li>
+          <li>
+            <a
+              className="LoginActions__item-anchor"
+              href={`https://www.sussexstudent.com/login`}
+            >
+              Other users
+            </a>
           </li>
         </ul>
+        <a href="https://www.sussexstudent.com/login">Register</a>
       </Modal>
     );
   }

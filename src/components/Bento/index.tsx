7cc984ada7a0b2f0e2e0adb5806e51b49d0dc8f8
James Canning <james@brudil.com>
2018-06-19 11:15:20 +0100

    Use two shadows instead of a border
    Closes #368

--- src/components/Bento/index.tsx
+++ src/components/Bento/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BentoBoxImpulse } from '~components/Bento/BentoBoxImpulse';
 import { HighlightTextBox } from '~components/Bento/treatments/HighlightTextBox';
-import { VoteNowBox } from '~components/Bento/treatments/VoteNowBox';
+import {StudentAwardsBox} from "~components/Bento/treatments/StudentAwardsBox";
 
 const Bento: React.SFC<{}> = () => (
   <div className="Bento Trail">
     <div className="Trail__row Trail__row--21 Bento__main-row">
-      <VoteNowBox
-        link="https://www.sussexstudent.com/referenda/"
-        targetDate={new Date(2018, 4, 4, 17, 0, 0)}
-      />
+      <StudentAwardsBox />
       <HighlightTextBox
         link="https://www.surveymonkey.co.uk/r/MW22Y5Y"
         imageUrl="original_images/e8236f9aa8194f56a17b90764bd6aadd"

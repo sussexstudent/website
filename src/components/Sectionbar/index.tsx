f986ee9e02b3d48fdf28b403a49ff6a2b3e402b3
James Canning <james@brudil.com>
2018-08-14 10:18:23 +0100

    Support title links in Sectionbar

--- src/components/Sectionbar/index.tsx
+++ src/components/Sectionbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from '@reach/router';
 
 export const SectionbarItem: React.SFC = (props) => (
   <li className="Sectionbar__menu-item">{props.children}</li>
@@ -6,13 +7,14 @@ export const SectionbarItem: React.SFC = (props) => (
 
 interface SectionbarProps {
   title: string;
+  titleLink?: string;
 }
 
 export const Sectionbar: React.SFC<SectionbarProps> = (props) => {
   return (
     <div className="Sectionbar">
       <div className="LokiContainer Sectionbar__container">
-        <h2 className="Sectionbar__title">{props.title}</h2>
+        <h2 className="Sectionbar__title">{props.titleLink ? <Link to={props.titleLink}>{props.title}</Link> : props.title}</h2>
 
         <ul className="Sectionbar__menu type-pica">{props.children}</ul>
       </div>

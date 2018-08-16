68fc598692987d0ff0eca83c5665cbd2b0e0ae51
James Canning <james@brudil.com>
2018-08-16 23:08:04 +0100

    Move to using babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const plugins = ["babel-plugin-lodash", "@babel/plugin-syntax-dynamic-import"];
+  const presets = ["@babel/preset-react"];
+
+  if (process.env.BABEL_ENV === 'bundle') {
+    presets.push(["@babel/preset-env", {
+      "targets": {
+        "browsers": ["last 2 versions", "safari 7", "IE 11"]
+      },
+      "useBuiltIns": false,
+      "modules": false,
+      "exclude": [
+        "transform-regenerator"
+      ]
+    }]);
+  } else if (process.env.BABEL_ENV === 'comp') {
+    presets.push(["@babel/preset-env", {
+      "targets": {
+        "node": "10"
+      },
+      "useBuiltIns": false,
+      "modules": false,
+      "exclude": [
+        "transform-regenerator"
+      ]
+    }]);
+  }
+
+  return {
+    presets,
+    plugins,
+  }
+};

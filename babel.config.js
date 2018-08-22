983cf150bd4a0f2b29f26ae2d76794c338c96bb1
James Canning <james@brudil.com>
2018-08-22 14:30:58 +0100

    Update target env

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
 
   const TARGET_PLATFORM = process.env.BABEL_ENV;
-  const TARGET_ENV = process.env.BABEL_ENV;
+  const TARGET_ENV = process.env.NODE_ENV;
 
   const plugins = ["babel-plugin-lodash", "@babel/plugin-syntax-dynamic-import", "@babel/plugin-proposal-object-rest-spread"];
   const presets = ["@babel/preset-react"];

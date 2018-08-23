791061b821bfe4827a54812718ad11c0396c512b
James Canning <james@brudil.com>
2018-08-23 12:13:38 +0100

    Restyle Sectionbar

--- src/components/Sectionbar/index.tsx
+++ src/components/Sectionbar/index.tsx
@@ -1,29 +1,99 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { COLORS, MQ, TYPE_SECONDARY } from '~libs/style';
+import styled from 'react-emotion';
 
-export const SectionbarItem: React.SFC = (props) => (
-  <li className="Sectionbar__menu-item">{props.children}</li>
-);
+export const SectionbarItem = styled('li')({
+  display: 'flex',
+  alignItems: 'center',
+
+  '& &--disabled': {
+    '& > a, & > button': {
+      color: COLORS.GREY_WINTER,
+      fontWeight: 'normal',
+      cursor: 'default',
+    },
+  },
+
+  '& > a, & > button': {
+    fontFamily: TYPE_SECONDARY,
+    color: COLORS.GREY_SLATE,
+    textDecoration: 'none',
+    fontWeight: 600,
+    padding: '0.2rem',
+
+    [MQ.Medium]: {
+      padding: '0 0.5rem',
+    },
+
+    '&.active': {
+      color: COLORS.BRAND_BLUE,
+    },
+  },
+});
 
 interface SectionbarProps {
   title: string;
   titleLink?: string;
 }
 
+const Root = styled('div')({
+  fontFamily: TYPE_SECONDARY,
+  padding: '0.6rem 0 0.3rem',
+  marginBottom: '1rem',
+  background: COLORS.GREY_SUMMER,
+  borderBottom: `1px solid ${COLORS.GREY_SPRING}`,
+  marginTop: '-1rem',
+});
+
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  [MQ.Medium]: {
+    flexDirection: 'row',
+  },
+});
+
+const Title = styled('h2')({
+  margin: '0 1rem 0 0',
+  color: COLORS.GREY_SAD_SLATE,
+  textAlign: 'center',
+  [MQ.Medium]: {
+    textAlign: 'left',
+  },
+  '& a': {
+    textDecoration: 'none',
+    color: COLORS.GREY_SAD_SLATE,
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+});
+
+const Menu = styled('ul')({
+  display: 'flex',
+  padding: 0,
+  margin: 0,
+  listStyle: 'none',
+  flexDirection: 'column',
+  [MQ.Medium]: {
+    flexDirection: 'row',
+  },
+});
+
 export const Sectionbar: React.SFC<SectionbarProps> = (props) => {
   return (
-    <div className="Sectionbar">
-      <div className="LokiContainer Sectionbar__container">
-        <h2 className="Sectionbar__title">
+    <Root>
+      <Container className="LokiContainer">
+        <Title>
           {props.titleLink ? (
             <Link to={props.titleLink}>{props.title}</Link>
           ) : (
             props.title
           )}
-        </h2>
-
-        <ul className="Sectionbar__menu type-pica">{props.children}</ul>
-      </div>
-    </div>
+        </Title>
+        <Menu>{props.children}</Menu>
+      </Container>
+    </Root>
   );
 };

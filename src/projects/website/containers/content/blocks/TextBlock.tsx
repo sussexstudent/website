d74703594472bbc8416cb4f32d12747d2b9cc0a4
James Canning <james@brudil.com>
2018-09-26 14:49:35 +0100

    Skip empty elements in htmlr

--- src/projects/website/containers/content/blocks/TextBlock.tsx
+++ src/projects/website/containers/content/blocks/TextBlock.tsx
@@ -7,11 +7,12 @@ import { getTextFromElementChildren } from '~website/containers/content/utils';
 export const TextBlock: StreamFieldBlock<{ value: string }> = ({ block }) => {
   const html = convert(block.value, {
     transform: {
-      h2: (props) => (
+      h2: (props) => props.children === null ? null : (
         <h2 {...props} id={slugify(getTextFromElementChildren(props.children))}>
           {props.children}
         </h2>
       ),
+      p: (props) => props.children === null ? null : <p>{props.children}</p>
     },
   });
   return <div className="Prose type-body-copy ContentBlock">{html}</div>;

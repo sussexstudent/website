95c8b7f5d842197ec63f96e029bb1ef864f90828
James Canning <james@brudil.com>
2018-11-09 10:11:23 +0000

    Fix relatedcontent bug

--- src/projects/website/components/RelatedContent/index.tsx
+++ src/projects/website/components/RelatedContent/index.tsx
@@ -14,9 +14,12 @@ export const RelatedContent = ({ relatedContent }: RelatedContentProps) => (
         <ul>
           {linkSection.value.links.map((link: any) => (
             <li>
-              <InternalAppLink key={link.id} to={link.value.link.path}>
-                {link.value.link.title}
-              </InternalAppLink>
+              {link.type === 'internal_link' && <InternalAppLink key={link.id} to={link.value.link.path}>
+                {link.value.title || link.value.link.title}
+              </InternalAppLink>}
+              {link.type === 'external_link' && <a key={link.id} href={link.value.link.path} target={link.value.target}>
+                {link.value.title}
+              </a>}
             </li>
           ))}
         </ul>

3c63b727c4703338e66ec66142a4dcc74350a40d
James Canning <james@brudil.com>
2018-11-22 12:38:14 +0000

    Fix ScrollToTop

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import {withRouter} from 'react-router';
 
-export class ScrollToTop extends React.Component {
-  componentDidMount() {
-    requestAnimationFrame(() => {
+class ScrollToTopComponent extends React.Component<any> {
+  componentDidUpdate(prevProps: any) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0);
-    });
+    }
   }
 
   render() {
     return this.props.children;
   }
 }
+
+export const ScrollToTop = withRouter(ScrollToTopComponent);

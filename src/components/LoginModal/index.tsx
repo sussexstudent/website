f5b58136b88a400e8da7c5d38e3e5e1da06d8c07
James Canning <james@brudil.com>
2019-02-22 14:50:23 +0000

    Simplify LoginModal

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -6,58 +6,56 @@ import getApolloClientForFalmer from '~libs/getApolloClientForFalmer';
 
 interface LoginModalProps {}
 
-export class LoginModal extends React.Component<
-  LoginModalProps & ReactModal.Props
-> {
-  render() {
-    const currentPath =
-      typeof window !== 'undefined' ? window.location.pathname : '/';
+export const LoginModal: React.FC<LoginModalProps & ReactModal.Props> = (
+  props,
+) => {
+  const currentPath =
+    typeof window !== 'undefined' ? window.location.pathname : '/';
 
-    return (
-      <Modal size="small" {...this.props} footerClose>
-        <h1 className="Modal__heading">Log in</h1>
-        <ApolloProvider client={getApolloClientForFalmer}>
-          <BannerOutlet outlet="login.modal.top" />
-        </ApolloProvider>
-        <ul className="LoginActions">
-          <li>
-            <a
-              className="LoginActions__item-anchor"
-              href={`https://www.sussexstudent.com/sso/login.ashx?idp=SUSSEX_SHIB&ReturnUrl=${currentPath}`}
-            >
-              Sussex Students
-            </a>
-          </li>
-          <li>
-            <a
-              className="LoginActions__item-anchor"
-              href={`https://www.sussexstudent.com/sso/login.ashx?idp=BSMS_SHIB&ReturnUrl=${currentPath}`}
-            >
-              BSMS Students
-            </a>
-          </li>
-          <li>
-            <a
-              className="LoginActions__item-anchor"
-              href={`https://www.sussexstudent.com/login`}
-            >
-              Study Group & Gothenburg students
-            </a>
-          </li>
-          <li>
-            <a
-              className="LoginActions__item-anchor"
-              href={`https://www.sussexstudent.com/login`}
-            >
-              Other users
-            </a>
-          </li>
-        </ul>
+  return (
+    <Modal size="small" {...props} footerClose>
+      <h1 className="Modal__heading">Log in</h1>
+      <ApolloProvider client={getApolloClientForFalmer}>
+        <BannerOutlet outlet="login.modal.top" />
+      </ApolloProvider>
+      <ul className="LoginActions">
+        <li>
+          <a
+            className="LoginActions__item-anchor"
+            href={`https://www.sussexstudent.com/sso/login.ashx?idp=SUSSEX_SHIB&ReturnUrl=${currentPath}`}
+          >
+            Sussex Students
+          </a>
+        </li>
+        <li>
+          <a
+            className="LoginActions__item-anchor"
+            href={`https://www.sussexstudent.com/sso/login.ashx?idp=BSMS_SHIB&ReturnUrl=${currentPath}`}
+          >
+            BSMS Students
+          </a>
+        </li>
+        <li>
+          <a
+            className="LoginActions__item-anchor"
+            href={`https://www.sussexstudent.com/login`}
+          >
+            Study Group & Gothenburg students
+          </a>
+        </li>
+        <li>
+          <a
+            className="LoginActions__item-anchor"
+            href={`https://www.sussexstudent.com/login`}
+          >
+            Other users
+          </a>
+        </li>
+      </ul>
 
-        <ApolloProvider client={getApolloClientForFalmer}>
-          <BannerOutlet outlet="login.modal.bottom" />
-        </ApolloProvider>
-      </Modal>
-    );
-  }
-}
+      <ApolloProvider client={getApolloClientForFalmer}>
+        <BannerOutlet outlet="login.modal.bottom" />
+      </ApolloProvider>
+    </Modal>
+  );
+};

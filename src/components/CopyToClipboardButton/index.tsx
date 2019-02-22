01f4763c250de17f9f42d8aa71435b0bb5220a3d
James Canning <james@brudil.com>
2019-02-22 14:48:25 +0000

    Simplify CopyToClipboard

--- src/components/CopyToClipboardButton/index.tsx
+++ src/components/CopyToClipboardButton/index.tsx
@@ -1,36 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 interface IProps {
   value: string;
 }
 
-interface IState {
-  hasCopied: boolean;
-}
+const CopyToClipboardButton: React.FC<IProps> = ({ value, children }) => {
+  const [hasCopied, setHasCopied] = useState(false);
 
-class CopyToClipboardButton extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-
-    this.state = {
-      hasCopied: false,
-    };
-  }
-
-  render() {
-    const { hasCopied } = this.state;
-    return (
-      <CopyToClipboard
-        text={this.props.value}
-        onCopy={() => this.setState({ hasCopied: true })}
-      >
-        <button className="Button">
-          {hasCopied ? 'Copied!' : this.props.children}
-        </button>
-      </CopyToClipboard>
-    );
-  }
-}
+  return (
+    <CopyToClipboard text={value} onCopy={() => setHasCopied(true)}>
+      <button className="Button">{hasCopied ? 'Copied!' : children}</button>
+    </CopyToClipboard>
+  );
+};
 
 export default CopyToClipboardButton;

def37737fc28e07251ee11ce042ccd353db8a107
James Canning <james@brudil.com>
2019-02-23 00:21:58 +0000

    Simplify ContentPage

--- src/projects/website/containers/content/ContentPage.tsx
+++ src/projects/website/containers/content/ContentPage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { History } from 'history';
 import CONTENT_PAGE_QUERY from './ContentPageQuery.graphql';
 import ctRoutes from '~website/containers/content/contentTypeRoutes';
-import { HandledQuery } from '~components/HandledQuery';
 import Helmet from 'react-helmet';
 import { FourOhFourPage } from './FourOhFourPage';
 import { Switch } from 'react-router';
+import { useQuery } from 'react-apollo-hooks';
 
 interface OwnProps {
   path: string;
@@ -23,61 +23,53 @@ interface Result {
   };
 }
 
-class ContentPageQuery extends HandledQuery<Result, {}> {}
-
 type IProps = OwnProps;
 
 const ContentPage: React.FC<IProps> = (props: IProps) => {
+  const { data, loading } = useQuery<Result>(CONTENT_PAGE_QUERY, {
+    variables: {
+      path: props.path,
+    },
+  });
+
+  if (!data || loading) {
+    return null;
+  }
+
+  const page = data.page;
+
+  if (page === null) {
+    return <FourOhFourPage />;
+  }
+
+  if (page.contentType === 'StubPage' && props.history) {
+    props.history.replace(`/browse${props.path}`, { replace: true });
+    return null;
+  }
+  const RoutesForContentType = ctRoutes.hasOwnProperty(page.contentType)
+    ? ctRoutes[page.contentType]
+    : null;
+
+  if (RoutesForContentType) {
+    return (
+      <React.Fragment>
+        <Helmet title={page.seoTitle || page.title}>
+          {page.searchDescription && (
+            <meta name="description" content={page.searchDescription} />
+          )}
+        </Helmet>
+
+        <Switch>
+          <RoutesForContentType page={page} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <ContentPageQuery
-      query={CONTENT_PAGE_QUERY}
-      variables={{
-        path: props.path,
-      }}
-    >
-      {({ data }) => {
-        if (!data) {
-          return;
-        }
-
-        const page = data.page;
-
-        if (page === null) {
-          return <FourOhFourPage />;
-        }
-
-        if (page.contentType === 'StubPage' && props.history) {
-          props.history.replace(`/browse${props.path}`, { replace: true });
-          return null;
-        }
-
-        const RoutesForContentType = ctRoutes.hasOwnProperty(page.contentType)
-          ? ctRoutes[page.contentType]
-          : null;
-
-        if (RoutesForContentType) {
-          return (
-            <React.Fragment>
-              <Helmet title={page.seoTitle || page.title}>
-                {page.searchDescription && (
-                  <meta name="description" content={page.searchDescription} />
-                )}
-              </Helmet>
-
-              <Switch>
-                <RoutesForContentType page={page} />
-              </Switch>
-            </React.Fragment>
-          );
-        }
-
-        return (
-          <div className="Layout">
-            <h1>Page type can't be found: "{page.contentType}"</h1>
-          </div>
-        );
-      }}
-    </ContentPageQuery>
+    <div className="Layout">
+      <h1>Page type can't be found: "{page.contentType}"</h1>
+    </div>
   );
 };
 

b5016b8d7368883504a4781fa74ffac6d2bd6b1c
James Canning <james@brudil.com>
2019-02-23 21:28:17 +0000

    Remove legacy banners

--- src/components/Bento/Bento.stories.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Bento } from '~components/Bento/index';
-
-storiesOf('Bento', module).add('full', () => <Bento />);

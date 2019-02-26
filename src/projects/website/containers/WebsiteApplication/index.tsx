47500798991a409b1dfcb3887bcf49ced5e225cb
James Canning <james@brudil.com>
2019-02-26 00:27:02 +0000

    Simplify WebsiteApplication

--- src/projects/website/containers/WebsiteApplication/index.tsx
+++ src/projects/website/containers/WebsiteApplication/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { History, Location } from 'history';
+import React, { useEffect } from 'react';
 import BookMarketApp from '~website/containers/bookmarket/BookMarketApp';
 import EventsApplication from '~website/containers/EventsApplication';
 import * as routerActions from '~website/ducks/router';
 import { AppMountState } from '~website/ducks/router';
-import { connect } from 'react-redux';
 import { ContentPage } from '~website/containers/content/ContentPage';
-import { compose } from 'recompose';
 import { ContentExplorer } from '~website/layouts/ContentExplorer';
 import { LoadableLoading } from '~components/LoadableLoading';
 import Loadable from 'react-loadable';
@@ -18,15 +15,18 @@ import { FreshersContentAPI } from '~website/containers/freshers/ContentAPIConta
 import { FreshersEvents } from '~website/containers/freshers/FreshersEvents';
 import { RouterAnalytics } from '~components/RouterAnalytics';
 import { FourOhFourPage } from '~website/containers/content/FourOhFourPage';
-import { Route, Switch, Redirect, withRouter } from 'react-router';
+import {
+  Route,
+  Switch,
+  Redirect,
+  withRouter,
+  RouteComponentProps,
+} from 'react-router';
 import { ScrollToTop } from '~components/ScrollToTop';
+import { useDispatch } from 'redux-react-hook';
 
-interface WebsiteApplicationProps {
-  setRouter: typeof routerActions.setRouter;
-  announceMount: typeof routerActions.announceMount;
-  history: History;
+interface WebsiteApplicationProps extends RouteComponentProps {
   appMountState: AppMountState;
-  location: Location;
 }
 
 const ContentAPI = (props: any) => (
@@ -53,80 +53,64 @@ const LoadableContentBrowser = Loadable({
 
 const FourOhFourPageRoute = FourOhFourPage as any;
 
-class WebsiteApplication extends React.Component<WebsiteApplicationProps> {
-  componentDidMount() {
-    this.props.setRouter(this.props.history, this.props.location);
-    this.props.announceMount(this.props.appMountState);
-  }
+const WebsiteApplication: React.FC<WebsiteApplicationProps> = ({
+  history,
+  location,
+  appMountState,
+}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(routerActions.setRouter(history, location));
+  }, [history, location]);
+  useEffect(() => {
+    dispatch(routerActions.announceMount(appMountState));
+  }, []);
 
-  componentDidUpdate() {
-    this.props.setRouter(this.props.history, this.props.location);
-  }
+  return (
+    <ScrollToTop>
+      <Helmet
+        defaultTitle="Sussex Students' Union"
+        titleTemplate="%s | Sussex Students' Union"
+      />
+      <RouterAnalytics />
+      <ErrorBoundary>
+        <Switch>
+          <Route component={Homepage} path="/" exact />
+          <Route component={BookMarketApp} path="/book-market" />
+          <Route component={LoadableContentBrowser} path="/browse" />
+          <Route component={EventsApplication} path="/whats-on" />
+          <Route component={LoadableSearchApp} path="/search" />
+          <Route
+            component={LoadableStudentGroupsDiscovery}
+            path="/sport-societies-media/discover"
+          />
+          <Redirect from="/freshers/freshers-week-events/" to="/freshers" />
+          <Route component={FreshersEvents} path="/freshers/whats-on" />
+          <Route component={FreshersContentAPI} path="/freshers" />
+          {/*<EventDiscovery path="/event-discovery" />*/}
+          <Route component={ContentAPI} path="/get-involved" exact />
+          <Route
+            component={ContentAPI}
+            path="/get-involved/societies-and-student-media"
+          />
+          <Route component={ContentAPI} path="/get-involved/officers" />
+          <Route component={ContentAPI} path="/get-involved/sports-clubs" />
+          <Route component={ContentAPI} path="/get-involved/decision-making" />
+          <Route component={ContentAPI} path="/services" />
+          <Route component={ContentAPI} path="/support" exact />
+          <Route component={ContentAPI} path="/get-involved-next" />
+          <Route component={ContentAPI} path="/about-us-next" />
+          <Route
+            component={ContentAPI}
+            path="/get-involved/campaigns-toolkit"
+          />
+          <Route component={ContentAPI} path="/about-us/contact" />
+          <Route component={ContentExplorer} path="/content-explorer" exact />
+          <Route component={FourOhFourPageRoute} default />
+        </Switch>
+      </ErrorBoundary>
+    </ScrollToTop>
+  );
+};
 
-  render() {
-    return (
-      <ScrollToTop>
-        <Helmet
-          defaultTitle="Sussex Students' Union"
-          titleTemplate="%s | Sussex Students' Union"
-        />
-        <RouterAnalytics />
-        <ErrorBoundary>
-          <Switch>
-            <Route component={Homepage} path="/" exact />
-            <Route component={BookMarketApp} path="/book-market" />
-            <Route component={LoadableContentBrowser} path="/browse" />
-            <Route component={EventsApplication} path="/whats-on" />
-            <Route component={LoadableSearchApp} path="/search" />
-            <Route
-              component={LoadableStudentGroupsDiscovery}
-              path="/sport-societies-media/discover"
-            />
-            <Redirect from="/freshers/freshers-week-events/" to="/freshers" />
-            <Route component={FreshersEvents} path="/freshers/whats-on" />
-            <Route component={FreshersContentAPI} path="/freshers" />
-            {/*<EventDiscovery path="/event-discovery" />*/}
-            <Route component={ContentAPI} path="/get-involved" exact />
-            <Route
-              component={ContentAPI}
-              path="/get-involved/societies-and-student-media"
-            />
-            <Route component={ContentAPI} path="/get-involved/officers" />
-            <Route component={ContentAPI} path="/get-involved/sports-clubs" />
-            <Route
-              component={ContentAPI}
-              path="/get-involved/decision-making"
-            />
-            <Route component={ContentAPI} path="/services" />
-            <Route component={ContentAPI} path="/support" exact />
-            <Route component={ContentAPI} path="/get-involved-next" />
-            <Route component={ContentAPI} path="/about-us-next" />
-            <Route
-              component={ContentAPI}
-              path="/get-involved/campaigns-toolkit"
-            />
-            <Route component={ContentAPI} path="/about-us/contact" />
-            <Route component={ContentExplorer} path="/content-explorer" exact />
-            <Route component={FourOhFourPageRoute} default />
-          </Switch>
-        </ErrorBoundary>
-      </ScrollToTop>
-    );
-  }
-}
-
-export default compose<
-  WebsiteApplicationProps,
-  {
-    appMountState: AppMountState;
-  }
->(
-  withRouter,
-  connect(
-    null,
-    {
-      setRouter: routerActions.setRouter,
-      announceMount: routerActions.announceMount,
-    },
-  ),
-)(WebsiteApplication);
+export default withRouter(WebsiteApplication);

9ef142fcbf7e021276e6fb614238347128b05acb
James Canning <james@brudil.com>
2019-02-26 00:07:51 +0000

    Improve cypress test times by using JS

--- cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-const wp = require('@cypress/webpack-preprocessor')
-
-module.exports = (on) => {
-  const options = {
-    webpackOptions: require('../../config/webpack.dev.config'),
-  };
-  on('file:preprocessor', wp(options))
-};

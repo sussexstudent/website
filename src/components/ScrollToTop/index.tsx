c7b0c768dceae80b14c9cc81ba010a5b26cd37c6
James Canning <james@brudil.com>
2019-04-09 15:43:43 +0100

    Ensure ScrollToTop does not trigger on hash anchors

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
 
 const ScrollToTopComponent: React.FC<
   RouteComponentProps & { children: any }
 > = ({ history, children }) => {
+  const [currentKey, setKey] = useState(
+    `${history.location.pathname}${history.location.search}`,
+  );
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
+    const unlisten = history.listen((location) => {
+      const key = `${location.pathname}${location.search}`;
+      if (currentKey !== key) {
+        window.scrollTo(0, 0);
+        setKey(key);
+      }
     });
     return () => {
       unlisten();

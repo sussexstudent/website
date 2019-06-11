7fbe862254b36d38a093396ff3729f22abf0a343
Eira Rangel <eira.rangel@gmail.com>
2019-06-11 14:31:26 +0100

    Remove same related news article from news page

--- src/modules/homepageNews.js
+++ src/modules/homepageNews.js
@@ -5,6 +5,13 @@ import { parseNews } from '~libs/msl';
 
 export default () => {
   const articles = parseNews(document.body);
+  const news_pathname = window.location.pathname;
+  for (const article of articles) {
+    const index = article.link.indexOf('/news/');
+    if (article.link.substring(index) === news_pathname) {
+      articles.splice(article.id,1);
+    }
+  }
 
   ReactDOM.render(
     <NewsList items={articles} />,

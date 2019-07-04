d3de4561c22cda0231a39c9e5fcc3823a5a7a984
James Canning <james@brudil.com>
2019-07-04 14:30:06 +0100

    Add heading component

---
+++ packages/website/src/components/Heading.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { css, jsx } from '@emotion/core';
+import { type, Typeface, TypeSize } from '@ussu/common/src/libs/style/type';
+
+export enum HeadingLevel {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6',
+}
+
+export interface HeadingProps {
+  level: HeadingLevel;
+  size: TypeSize;
+}
+
+const headingStyle = css({
+  marginTop: 0,
+});
+
+export const Heading: React.FC<HeadingProps> = ({ level, children, size }) => {
+  return jsx(
+    level,
+    { css: [headingStyle, css(type(size, Typeface.Secondary))] },
+    children,
+  );
+};

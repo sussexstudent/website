dc36a3f8b3a5f6fc384d2ebc6311b8e6765543e4
James Canning <james@brudil.com>
2019-07-09 14:44:50 +0100

    Allow custom text within ErrorState

--- packages/website/src/components/ErrorState/index.tsx
+++ packages/website/src/components/ErrorState/index.tsx
@@ -4,12 +4,22 @@ import SadFaceIcon from '@ussu/common/src/icons/sad-face.svg';
 const refreshPage = () =>
   typeof window !== 'undefined' && window.location.reload();
 
-const ErrorState: React.FC<{}> = () => (
+interface ErrorStateProps {
+  title?: string;
+  subtitle?: string;
+  description?: string;
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({
+  title = 'Something went wrong.',
+  subtitle = 'Something went wrong while making this page.',
+  description = "We've been notified of what happened.",
+}) => (
   <div className="Stonewall">
     <SadFaceIcon className="Stonewall__image" />
-    <h1 className="type-canon">Something went wrong.</h1>
-    <p className="type-pica">Something went wrong while making this page.</p>
-    <p className="type-pica">We've been notified of what happened.</p>
+    <h1 className="type-canon">{title}</h1>
+    <p className="type-pica">{subtitle}</p>
+    <p className="type-pica">{description}</p>
     <button className="Button type-great-primer" onClick={refreshPage}>
       Refresh this page
     </button>

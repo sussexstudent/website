d666cdbc7b423660040f4c61bf94bf0cc7948b3e
James Canning <james@brudil.com>
2019-08-14 12:26:37 +0100

    Remove freshers splash feature flag

--- packages/website/src/containers/content/pages/FreshersSwitcher.tsx
+++ packages/website/src/containers/content/pages/FreshersSwitcher.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import { useFlag } from '../../../hooks/useFlag';
 import { FreshersHomepage } from './FreshersHomepage';
-import { FreshersSplash } from './FreshersSplash';
 
 export const FreshersSwitcher = (props: any) => {
-  const { loading, state } = useFlag('freshers.displaySplash');
-
-  if (loading) {
-    return null;
-  }
-
-  if (state && window.location.hash !== '#preview') {
-    return <FreshersSplash />;
-  }
-
   return <FreshersHomepage {...props} />;
 };

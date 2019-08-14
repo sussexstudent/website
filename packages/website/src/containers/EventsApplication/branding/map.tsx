acceadb1c687f554acd2a634a38ff8f62648bc26
James Canning <james@brudil.com>
2019-08-14 19:59:31 +0100

    Improve freshers event branding

--- packages/website/src/containers/EventsApplication/branding/map.tsx
+++ packages/website/src/containers/EventsApplication/branding/map.tsx
@@ -6,7 +6,15 @@ export const whatsOnBrandingMap: {
 } = {
   'freshers-week-2019': {
     [WhatsOnBrandedComponentLocation.Container]: ({ children }) => (
-      <div css={{ backgroundColor: 'rgb(250, 208, 15)' }}>{children}</div>
+      <div
+        css={{
+          backgroundImage: `url(${require('../../../img/freshers-2019/repeatable-bg-left.svg')}), url(${require('../../../img/freshers-2019/repeatable-bg-right.svg')})`,
+          backgroundRepeat: 'repeat-y, repeat-y',
+          backgroundPosition: 'top left, top right',
+        }}
+      >
+        {children}
+      </div>
     ),
     [WhatsOnBrandedComponentLocation.PeriodHeader]: ({
       brandingPeriod,

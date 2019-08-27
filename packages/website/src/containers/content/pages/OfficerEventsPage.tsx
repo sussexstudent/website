06a5608e997f0d87755f8c73f20f28d0ad5465c7
Eira Rangel <eira.rangel@gmail.com>
2019-08-27 16:20:14 +0100

    Add event listing and fields

--- packages/website/src/containers/content/pages/OfficerEventsPage.tsx
+++ packages/website/src/containers/content/pages/OfficerEventsPage.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Page } from '../types';
+import convert from 'htmr';
+import EventListingsQuery from '../../EventsCalender/EventListings.graphql';
+import { EventListings } from '../../EventsCalender/EventListings';
+import { useQuery } from '@apollo/react-hooks';
+import { startOfDay, addMonths } from 'date-fns/esm';
+import Loader from '../../../components/Loader';
 
 interface IOfficerEventsIndex extends Page<Page[]> {}
 
 interface IOfficerEventsPage extends Page {
   section: IOfficerEventsIndex;
+  description: string;
+  curator: {
+    id: BigInt;
+  };
+  filter: any;
 }
 
 export interface OfficerEventsPageProps {
   page: IOfficerEventsPage;
 }
 
-export const OfficerEventsPage: React.FC<OfficerEventsPageProps> = ({}) => {
-  return <div>test</div>;
+export const OfficerEventsPage: React.FC<OfficerEventsPageProps> = ({
+  page,
+}) => {
+  const [now] = useState(new Date());
+  const { data, loading } = useQuery(EventListingsQuery, {
+    variables: {
+      filter: page.filter || {
+        fromTime: startOfDay(now).toISOString(),
+        toTime: addMonths(startOfDay(now), 6).toISOString(),
+        curatedBy: page.curator.id,
+      },
+    },
+  });
+
+  if (loading) {
+    return <Loader dark />;
+  }
+
+  return (
+    <div className="LokiContainer">
+      <h1>{page.title}</h1>
+      {convert(page.description)}
+      <EventListings events={data.allEvents} removePast={true} />
+    </div>
+  );
 };

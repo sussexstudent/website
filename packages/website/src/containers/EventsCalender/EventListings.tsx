ac3adc682051e4ac1a7afa7a6719426188ce4fb9
James Canning <james@brudil.com>
2019-09-10 16:28:37 +0100

    Lazy render event listings

--- packages/website/src/containers/EventsCalender/EventListings.tsx
+++ packages/website/src/containers/EventsCalender/EventListings.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   getSmartDate,
   organisePartsForUI,
@@ -7,10 +7,27 @@ import {
 } from '../EventsApplication/utils';
 import { Event, EventPart } from '@ussu/common/src/types/events';
 import EventsCalenderItem from '../EventsCalender/EventsCalenderItem';
+import VisibilitySensor from 'react-visibility-sensor';
 
 interface EventListingsProps {
   events: any;
   removePast: boolean;
+}
+
+function bucketFill(buckets: number[], count: number) {
+  let countLeft = count;
+  let bucketsUsed = 0;
+  for (let bucket of buckets) {
+    console.log({ bucket, countLeft });
+    if (bucket <= countLeft) {
+      bucketsUsed = bucketsUsed + 1;
+      countLeft = countLeft - bucket;
+    } else {
+      break;
+    }
+  }
+
+  return [bucketsUsed, countLeft];
 }
 
 export const EventListings: React.FC<EventListingsProps> = (props) => {
@@ -21,37 +38,70 @@ export const EventListings: React.FC<EventListingsProps> = (props) => {
   }));
   // let previousDay = null;
   const eventParts = splitEventsInToParts(events, props.removePast);
-  const uiEvents = organisePartsForUI(eventParts, props.removePast);
+  const monthGroup = organisePartsForUI(eventParts, props.removePast);
   // chunk by day
+
+  const [displayLevel, setDisplayLevel] = useState(1);
+
+  console.log({
+    uiEvents: monthGroup,
+    displayLevel,
+    displaying: monthGroup.slice(0, displayLevel),
+  });
+
+  const monthDayCount = monthGroup.map((month) => month.parts.length);
+
+  const [monthDisplay, dayDisplay] = bucketFill(monthDayCount, displayLevel);
 
   return (
     <div className="LokiContainer">
       <div className="EventsCalender">
-        {uiEvents.map(({ sectionTitle, parts }) => (
-          // sectionTitle might not be unique in the future
-          <div className="EventsCalender__section" key={sectionTitle}>
-            <h2 className="EventsCalender__section-title">{sectionTitle}</h2>
-            {parts.map((chunk, index) => (
-              <div key={index}>
-                <h3 className={cx('EventsCalender__item-date-kicker')}>
-                  {getSmartDate(chunk[0])}
-                </h3>
-                <div className="EventsCalender__section-items">
-                  {chunk.map((part: EventPart, index: number) => {
-                    return (
-                      <div
-                        className="EventsCalender__part-container"
-                        key={index}
-                      >
-                        <EventsCalenderItem part={part} />
-                      </div>
-                    );
-                  })}
+        {monthGroup
+          .slice(0, monthDisplay + 1)
+          .map(({ sectionTitle, parts }, index) => (
+            // sectionTitle might not be unique in the future
+            <div className="EventsCalender__section" key={sectionTitle}>
+              <h2 className="EventsCalender__section-title">{sectionTitle}</h2>
+              {(index === monthDisplay
+                ? parts.slice(0, dayDisplay)
+                : parts
+              ).map((chunk, index) => (
+                <div key={index}>
+                  <h3 className={cx('EventsCalender__item-date-kicker')}>
+                    {getSmartDate(chunk[0])}
+                  </h3>
+                  <div className="EventsCalender__section-items">
+                    {chunk.map((part: EventPart, index: number) => {
+                      return (
+                        <div
+                          className="EventsCalender__part-container"
+                          key={index}
+                        >
+                          <EventsCalenderItem part={part} />
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))}
+        <VisibilitySensor
+          partialVisibility
+          onChange={() => setDisplayLevel((level) => level + 1)}
+        >
+          <div
+            style={{
+              display: 'block',
+              background: 'red',
+              width: 1,
+              height: 1000,
+              opacity: 0,
+              position: 'absolute',
+              bottom: 0,
+            }}
+          ></div>
+        </VisibilitySensor>
       </div>
     </div>
   );

cffec5c6b7013afd3ed232c761a290baae76e4ec
James Canning <james@brudil.com>
2019-09-19 10:41:18 +0100

    Disable lazy render

--- packages/website/src/containers/EventsCalender/EventListings.tsx
+++ packages/website/src/containers/EventsCalender/EventListings.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import React, { useState } from 'react';
+import React from 'react';
 import {
   getSmartDate,
   organisePartsForUI,
@@ -7,28 +7,28 @@ import {
 } from '../EventsApplication/utils';
 import { Event, EventPart } from '@ussu/common/src/types/events';
 import EventsCalenderItem from '../EventsCalender/EventsCalenderItem';
-import VisibilitySensor from 'react-visibility-sensor';
+// import VisibilitySensor from 'react-visibility-sensor';
 
 interface EventListingsProps {
   events: any;
   removePast: boolean;
 }
 
-function bucketFill(buckets: number[], count: number) {
-  let countLeft = count;
-  let bucketsUsed = 0;
-  for (let bucket of buckets) {
-    console.log({ bucket, countLeft });
-    if (bucket <= countLeft) {
-      bucketsUsed = bucketsUsed + 1;
-      countLeft = countLeft - bucket;
-    } else {
-      break;
-    }
-  }
-
-  return [bucketsUsed, countLeft];
-}
+// function bucketFill(buckets: number[], count: number) {
+//   let countLeft = count;
+//   let bucketsUsed = 0;
+//   for (let bucket of buckets) {
+//     console.log({ bucket, countLeft });
+//     if (bucket <= countLeft) {
+//       bucketsUsed = bucketsUsed + 1;
+//       countLeft = countLeft - bucket;
+//     } else {
+//       break;
+//     }
+//   }
+//
+//   return [bucketsUsed, countLeft];
+// }
 
 export const EventListings: React.FC<EventListingsProps> = (props) => {
   const events = props.events.edges.map(({ node }: { node: Event }) => ({
@@ -41,31 +41,22 @@ export const EventListings: React.FC<EventListingsProps> = (props) => {
   const monthGroup = organisePartsForUI(eventParts, props.removePast);
   // chunk by day
 
-  const [displayLevel, setDisplayLevel] = useState(1);
+  // const [displayLevel, setDisplayLevel] = useState(1);
 
-  console.log({
-    uiEvents: monthGroup,
-    displayLevel,
-    displaying: monthGroup.slice(0, displayLevel),
-  });
+  // const monthDayCount = monthGroup.map((month) => month.parts.length);
 
-  const monthDayCount = monthGroup.map((month) => month.parts.length);
-
-  const [monthDisplay, dayDisplay] = bucketFill(monthDayCount, displayLevel);
+  // const [monthDisplay, dayDisplay] = bucketFill(monthDayCount, displayLevel);
 
   return (
     <div className="LokiContainer">
       <div className="EventsCalender">
         {monthGroup
-          .slice(0, monthDisplay + 1)
-          .map(({ sectionTitle, parts }, index) => (
+          // .slice(0, monthDisplay + 1)
+          .map(({ sectionTitle, parts }) => (
             // sectionTitle might not be unique in the future
             <div className="EventsCalender__section" key={sectionTitle}>
               <h2 className="EventsCalender__section-title">{sectionTitle}</h2>
-              {(index === monthDisplay
-                ? parts.slice(0, dayDisplay)
-                : parts
-              ).map((chunk, index) => (
+              {parts.map((chunk, index) => (
                 <div key={index}>
                   <h3 className={cx('EventsCalender__item-date-kicker')}>
                     {getSmartDate(chunk[0])}
@@ -86,22 +77,22 @@ export const EventListings: React.FC<EventListingsProps> = (props) => {
               ))}
             </div>
           ))}
-        <VisibilitySensor
-          partialVisibility
-          onChange={() => setDisplayLevel((level) => level + 1)}
-        >
-          <div
-            style={{
-              display: 'block',
-              background: 'red',
-              width: 1,
-              height: 1000,
-              opacity: 0,
-              position: 'absolute',
-              bottom: 0,
-            }}
-          ></div>
-        </VisibilitySensor>
+        {/*<VisibilitySensor*/}
+        {/*  partialVisibility*/}
+        {/*  onChange={() => setDisplayLevel((level) => level + 1)}*/}
+        {/*>*/}
+        {/*  <div*/}
+        {/*    style={{*/}
+        {/*      display: 'block',*/}
+        {/*      background: 'red',*/}
+        {/*      width: 1,*/}
+        {/*      height: 1000,*/}
+        {/*      opacity: 0,*/}
+        {/*      position: 'absolute',*/}
+        {/*      bottom: 0,*/}
+        {/*    }}*/}
+        {/*  ></div>*/}
+        {/*</VisibilitySensor>*/}
       </div>
     </div>
   );

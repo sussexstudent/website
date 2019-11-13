599f7338080fa13fab3fb00f18fabec1e2271546
James Canning <james@brudil.com>
2019-11-13 17:35:29 +0000

    Add modal transition animation

--- packages/website/src/components/Modal/index.tsx
+++ packages/website/src/components/Modal/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import cx from 'classnames';
 import ReactModal from 'react-modal';
 import { omit } from 'lodash';
+import { COLORS, MQ, TYPE_PRIMARY } from '@ussu/basil/src/style';
 
 interface EnchancedModalProps {
   size?: 'small' | 'normal' | 'full';
@@ -13,18 +13,59 @@ const Modal = (
   props: ReactModal.Props & EnchancedModalProps & { children: any },
 ) => (
   <ReactModal
-    className={cx('Modal', {
-      'Modal--small': props.size === 'small',
-      'Modal--full': props.size === 'full',
-    })}
+    closeTimeoutMS={300}
+    css={[
+      {
+        position: 'relative',
+        width: '100%',
+        background: 'rgb(255, 255, 255)',
+        overflow: 'auto',
+        outline: 'none',
+        minHeight: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        flex: '1 1 auto',
+        [MQ.Medium]: {
+          width:
+            props.size === 'small'
+              ? 400
+              : props.size === 'full'
+              ? 'none'
+              : '80%',
+          maxWidth: 800,
+
+          marginTop: 60,
+          marginBottom: 60,
+          boxSizing: 'border-box',
+          boxShadow: '0 8px 15px 3px rgba(0, 0, 0, 0.2)',
+          borderRadius: 2,
+          flex: 'none',
+          minHeight: 0,
+        },
+      },
+    ]}
     overlayClassName="Overlay"
     {...(omit(props, ['size']) as ReactModal.Props)}
   >
-    <div className="Modal__content">{props.children}</div>
+    <div css={{ padding: '1rem' }}>{props.children}</div>
 
     {props.footerClose ? (
       <button
-        className="Modal__footerClose"
+        css={{
+          background: COLORS.GREY_SPRING,
+          color: COLORS.GREY_SAD_SLATE,
+          textAlign: 'center',
+          padding: '0.6rem',
+          boxSizing: 'border-box',
+          fontSize: '1.2rem',
+          width: '100%',
+          display: 'block',
+          fontFamily: TYPE_PRIMARY,
+          border: 0,
+          fontWeight: 'bold',
+          alignSelf: 'flex-end',
+          cursor: 'pointer',
+        }}
         onClick={(e) => (props.onRequestClose ? props.onRequestClose(e) : null)}
       >
         close

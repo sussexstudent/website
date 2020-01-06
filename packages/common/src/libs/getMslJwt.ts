f2221297a79993ed8bd029b6c1ab4ad1a8bae9d2
Eira Rangel <eira.rangel@gmail.com>
2020-01-06 12:02:57 +0000

    Fix issues with recent update

--- packages/common/src/libs/getMslJwt.ts
+++ packages/common/src/libs/getMslJwt.ts
@@ -1,7 +1,9 @@
+import { has, get } from 'lodash';
+
 export function getMslJwt(): string | false {
   return (
     localStorage.getItem('MSL_JWT_OVERRIDE') ||
-    (window.mslUserInfo && window.mslUserInfo.jwt) ||
+    (has(window, 'mslUserInfo') && get(window, 'mslUserInfo.jwt')) ||
     false
   );
 }
